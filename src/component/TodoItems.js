286c7f29b4c60e368be8b676a829e2c70a3499d3
Xiaoting Lin <xiaotinglin@Xiaotings-MacBook-Pro.local>
2020-06-23 20:54:41 -0700

    replace conditional render

--- src/component/TodoItems.js
+++ src/component/TodoItems.js
@@ -23,80 +23,35 @@ const TodoItems = () => {
 
   let newItems = items.filter(item => item.text.indexOf(searchText) !== -1);
 
-  return newItems.map(item => {
-    if (item.isEdit) {
-      return (
-        <li key={item.key}>
-          <div className="todo-item">
-            <form onSubmit={e => { e.preventDefault(); dispatch(sortTodo()) }}>
-              <input
-                type="number"
-                min="1"
-                defaultValue={item.order}
-                onChange={e => dispatch(inputOrder(e.target.value, item.key))}
-                onBlur={e => { e.preventDefault(); dispatch(sortTodo()) }}
-              />
-            </form>
-            <input
-              onChange={e => dispatch(updateTodo(e.target.value, item.key))}
-              defaultValue={item.text}
-            />
-            <div>
-              <button onClick={() => dispatch(cancelEdit(item.key))}>cancel</button>
-              <button onClick={() => dispatch(saveEdit(item.key))}>save</button>
-              <button onClick={() => dispatch(delTodo(item.key))}>delete</button>
-            </div>
-          </div>
-        </li>
-      );
-    } else if (item.isDone) {
-      return (
-        <li key={item.key}>
-          <div className="todo-item">
-            <form onSubmit={e => { e.preventDefault(); dispatch(sortTodo()) }}>
-              <input
-                type="number"
-                min="1"
-                defaultValue={item.order}
-                onChange={e => dispatch(inputOrder(e.target.value, item.key))}
-                onBlur={e => { e.preventDefault(); dispatch(sortTodo()) }}
-              />
-            </form>
-            <span className="line-through"
-              onClick={() => dispatch(markDone(item.key))}
-            >
-              {item.text}
-            </span>
-            <div>
-              <button disabled>edit</button>
-              <button onClick={() => dispatch(delTodo(item.key))}>delete</button>
-            </div>
-          </div>
-        </li>
-      );
-    } else {
-      return (
-        <li key={item.key}>
-          <div className="todo-item">
-            <form onSubmit={e => { e.preventDefault(); dispatch(sortTodo()) }}>
-              <input
-                type="number"
-                min="1"
-                defaultValue={item.order}
-                onChange={e => dispatch(inputOrder(e.target.value, item.key))}
-                onBlur={e => { e.preventDefault(); dispatch(sortTodo()) }}
-              />
-            </form>
-            <span onClick={() => dispatch(markDone(item.key))}>{item.text}</span>
-            <div>
-              <button onClick={() => dispatch(editTodo(item.key))}>edit</button>
-              <button onClick={() => dispatch(delTodo(item.key))}>delete</button>
-            </div>
-          </div>
-        </li>
-      );
-    }
-  });
+  return newItems.map(item => (
+    <li key={item.key}>
+      <div className="todo-item">
+        <input
+          type="number"
+          min="1"
+          defaultValue={item.order}
+          onChange={e => dispatch(inputOrder(e.target.value, item.key))}
+          onBlur={() => dispatch(sortTodo())}
+          onKeyPress={() => dispatch(sortTodo())}
+        />
+        <span
+          className={(item.isEdit && item.isDone === false) ? "none" : (item.isEdit === false && item.isDone) ? "block line-through" : ""}
+          onClick={() => dispatch(markDone(item.key))}
+        >{item.text}</span>
+        <input
+          className={item.isEdit ? "block" : "none"}
+          onChange={e => dispatch(updateTodo(e.target.value, item.key))}
+          defaultValue={item.text}
+        />
+        <div>
+        <button className={item.isEdit ? "block" : "none"} onClick={() => dispatch(cancelEdit(item.key))}>cancel</button>
+        <button className={item.isEdit ? "block" : "none"} onClick={() => dispatch(saveEdit(item.key))}>save</button>
+        <button className={item.isEdit ? "none" : "block"} disabled={item.isDone} onClick={() => dispatch(editTodo(item.key))}>edit</button>
+        <button onClick={() => dispatch(delTodo(item.key))}>delete</button>
+        </div>
+      </div>
+    </li>
+  ))
 };
 
 export default TodoItems;
